3fa8ba508cfef0db8e01d4e4a3a0b0d7df9b0020
CoderDan1983 <seekingsomething121@gmail.com>
2021-09-16 09:42:16 -0700

    saving project startup bare bones

---
+++ server.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const multer = require("multer");
+const upload = multer();
+require('dotenv').config();
+
+app.use('/', express.static(path.join(__dirname, '/')));
+
+
+app.listen(PORT, ()=>{
+	console.log('the Daniel is studying :)')
+});
+
+
+app.use('/form', upload.none(), (req,res,next)=>{
+	console.log('walked through here!')
+	next();
+});
+
+
+
+app.post('/grabTable', (req,res,next)=>{
+	let arrivingData='';
+	req.on("data", (data)=>{
+		arrivingData+=data;
+	});
+	req.on("end", ()=>{
+		const body=JSON.parse(arrivingData);
+		console.log(body);
+		pool.getConnection(function(err,con){
+			var query="SELECT * FROM " + body.table;
+			console.log(query)
+			con.query(query, function(err, rows){
+				console.log('55!')
+				console.log(rows);
+				con.release();
+			});
+		});
+	});
+
+});
